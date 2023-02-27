a0ff2a1fbc5e597f1c2ef4c42a373f6cb3eac26b
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-02-27 09:36:32 +0530

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './Main'
-
+import logo from './assets/LOGO.png'
 class App extends Component {
   constructor(props) {
     super(props)
@@ -96,6 +96,23 @@ class App extends Component {
   render() {
     return (
       <div>
+        {/* NavBar */}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            {/* logo */}
+            <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
+            <a className="navbar-brand" href="#">ClaimChain Insurance Marketplace</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        
         <main role="main">
           {this.state.loading
             ?
