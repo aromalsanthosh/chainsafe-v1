82c97e8d490db4929ca680f0824c306fa56957b1
aromalsanhosh <aromalsanthosh@protonmail.com>
2023-04-02 15:16:37 +0530

    fix: Navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Insurance from "../abis/Insurance.json";
 import Main from "./Main";
 import { useState, useEffect } from "react";
+import logo from "../assets/LOGO.png";
 
 function App() {
   const [account, setAccount] = useState("");
@@ -195,6 +196,21 @@ function App() {
   };
   return (
     <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            {/* logo */}
+            <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
+            <a className="navbar-brand" href="#">ClaimChain Insurance Marketplace</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </div>
+            </div>
+          </div>
+        </nav>
       <main role="main">
         {loading ? (
           <div id="loader" className="text-center mt-5">
